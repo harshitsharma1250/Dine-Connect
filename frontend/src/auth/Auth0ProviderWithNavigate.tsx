634d49f4269b73ffab9eb0b1824ed38468883be5
Harshit Sharma <harshitshrm8@gmail.com>
2024-07-24 21:59:58 +0530

    Commiting chages

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -6,11 +6,13 @@ type Props = {
 }
 const Auth0ProviderWithNavigate = ({children}: Props) => {
     const navigate = useNavigate() ;
+
     const domain = import.meta.env.VITE_AUTH0_DOMAIN ;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID ;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URI; 
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE ;
     
-    if(!domain || !clientId || !redirectUri){
+    if(!domain || !clientId || !redirectUri || !audience){
         throw new Error("unable to initialise auth") ;
     }
 
@@ -24,6 +26,7 @@ const Auth0ProviderWithNavigate = ({children}: Props) => {
             clientId={clientId} 
             authorizationParams={{
                 redirect_uri: redirectUri,
+                audience,
             }}
             onRedirectCallback={onRedirectCallback}
             >
